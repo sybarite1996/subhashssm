e24161471d2b2d75ffdd44bab2d2fc2694f16828
sybarite1996 <73710585+sybarite1996@users.noreply.github.com>
2022-03-30 17:19:22 +0530

    creating policy in json

---
+++ craetepolicyjson.py
@@ -0,0 +1,62 @@
+import boto3
+import json
+
+my_conf = {
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "cloudwatch:PutMetricData",
+                "ds:CreateComputer",
+                "ds:DescribeDirectories",
+                "ec2:DescribeInstanceStatus",
+                "logs:*",
+                "ssm:*",
+                "ec2messages:*"
+            ],
+            "Resource": "*"
+        },
+        {
+            "Effect": "Allow",
+            "Action": "iam:CreateServiceLinkedRole",
+            "Resource": "arn:aws:iam::*:role/aws-service-role/ssm.amazonaws.com/AWSServiceRoleForAmazonSSM*",
+            "Condition": {
+                "StringLike": {
+                    "iam:AWSServiceName": "ssm.amazonaws.com"
+                }
+            }
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "iam:DeleteServiceLinkedRole",
+                "iam:GetServiceLinkedRoleDeletionStatus"
+            ],
+            "Resource": "arn:aws:iam::*:role/aws-service-role/ssm.amazonaws.com/AWSServiceRoleForAmazonSSM*"
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "ssmmessages:CreateControlChannel",
+                "ssmmessages:CreateDataChannel",
+                "ssmmessages:OpenControlChannel",
+                "ssmmessages:OpenDataChannel"
+            ],
+            "Resource": "*"
+        }
+    ]
+}
+AWS_REGION = "ap-south-1"
+
+ssm_client = boto3.client("ssm", region_name=AWS_REGION)
+
+new_list_parameter = ssm_client.put_parameter(
+    Name='stringlistparameterpract',
+    Description='saving mltiple strings',
+    Value=json.dumps(my_conf),
+    Type='StringList',
+    Overwrite=True,
+    Tier='Standard',
+    DataType='text'
+)
