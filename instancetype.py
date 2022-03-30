8f597453b28d974a002ed37d7fc2eaf4600b22ee
sybarite1996 <73710585+sybarite1996@users.noreply.github.com>
2022-03-30 17:24:05 +0530

    update t3 to t2

--- instancetype.py
+++ instancetype.py
@@ -7,7 +7,7 @@ ssm_client = boto3.client("ssm", region_name=AWS_REGION)
 new_string_parameter = ssm_client.put_parameter(
     Name='InstType',
     Description='EC2 Instance type for Dev environment',
-    Value='t3.micro',
+    Value='t2.micro',
     Type='String',
     Overwrite=True,
     Tier='Standard',
